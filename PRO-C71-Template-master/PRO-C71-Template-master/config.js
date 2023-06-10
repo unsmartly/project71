d2246799f48deedaa11e9fb1bd13efe1ac21d6b8
unsmartly <111931454+unsmartly@users.noreply.github.com>
2023-06-10 11:55:37 -0700

    Add files via upload

---
+++ PRO-C71-Template-master/PRO-C71-Template-master/config.js
@@ -0,0 +1,16 @@
+import firebase from "firebase";
+require("@firebase/firestore");
+
+//Paste your firebaseConfig here
+const firebaseConfig = {
+    apiKey: "AIzaSyCPpUxVfqKbmZj3XY04Pek_fBDMFal4x5g",
+    authDomain: "ride-72c35.firebaseapp.com",
+    projectId: "ride-72c35",
+    storageBucket: "ride-72c35.appspot.com",
+    messagingSenderId: "1004308768489",
+    appId: "1:1004308768489:web:08dc94be9949472cdff278"
+  };
+//
+firebase.initializeApp(firebaseConfig);
+
+export default firebase.firestore();
